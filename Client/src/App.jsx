84c1ab7c556f1b38ade1446b510460408006a0ee
Osamadev168 <osamatwenty@gmail.com>
2022-12-21 20:18:36 +0500

    commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
     setProgress(true);
     try {
       const response = await fetch(
-        "https://openai-dall-e-production.up.railway.app/genImage",
+        "https://openai-dall-e-production.up.railway.app/",
         {
           method: "POST",
           headers: {
